08ba7d16700a6f296a513b31248bc6524240adf7
krydev <kryitdev@gmail.com>
2019-04-25 18:56:56 +0300

    Add topic selection option

--- frontend/js/gui.js
+++ frontend/js/gui.js
@@ -13,7 +13,7 @@ $(function(){
 /*topics and questions*/
 var questions_test = {
 
-  technology: ['Is human cloning justified, and should it be allowed?', 'What pros and cons of the advent of the AI(Artificial Intelligence)?', 'Will virtual reality replace the actual one?', 'Does our speed of progress increase with time?', 'Is there a limit in scientific discoveries?', 'Do technologies break the bond of renewal between humans and nature?', 'Can you think of any technology that has only made the world worse?', 'Should animals be used for scientific experiments?'],
+  technologies: ['Is human cloning justified, and should it be allowed?', 'What pros and cons of the advent of the AI(Artificial Intelligence)?', 'Will virtual reality replace the actual one?', 'Does our speed of progress increase with time?', 'Is there a limit in scientific discoveries?', 'Do technologies break the bond of renewal between humans and nature?', 'Can you think of any technology that has only made the world worse?', 'Should animals be used for scientific experiments?'],
   medicine: ['Should marijuana be a medical option?', 'What was the most significant achievement in overcoming the diseases?', 'What would the world be today without antibiotics?', 'Do you believe in a panacea?'],
   society: ['Are beauty pageants a way to objectifying women?', 'What can we do about racism?', 'Is there any way to decrease social stratification?', 'Why does the poverty still exist?', 'Where do you stand on LGBTQ community?', 'If you had to add something to humanity, what would your contribution be?', 'Are fandoms bad?', 'Is the death penalty appropriate?  Or should it be banned?', 'Which one is a more complicated gender: men or women?'],
   age: ['Have you ever noticed generation gap?', 'Is it right to say: wisdom comes with aging?', 'Are there some advantages of being old?', 'What are the most vital factors in aging?', 'Should we decrease the general retirement age?'],
@@ -46,8 +46,7 @@ var pickProperty = function (obj, obj2) { //picking a first element, then replac
   return obj2[arrayOfTopics[0]]
 }
 
-var new_arr = pickProperty(shuffled, questions_test).slice(0)
-var duplicate = new_arr.slice(0)
+var new_arr = pickProperty(shuffled, questions_test).slice(0);
 
 var getquestion = function () {
 
@@ -77,14 +76,23 @@ var anim_classes = {
 
 /******************/
 
-$('.primary,.tertiary').on('click', mainfunc)
+$('#primary-rnd-btn,.tertiary').on('click', function(){
+  mainfunc();
+})
+$('#primary-btn').click(function(){
+  mainfunc($( "#mainTopicBtn option:selected" ).text().toLowerCase());
+})
 
-function mainfunc () {
+function mainfunc (topic_name) {
 
-  $topic_selector.hide();
   $experiment.empty()
   $topic.show()
   $topic.removeClass(anim_classes.bounceOutLeft)
+  if (topic_name !== undefined){
+    arrayOfTopics = [topic_name];
+    new_arr = questions_test[topic_name]
+  }
+  
   getquestion()
   $topic.text(arrayOfTopics);
   if (!showntopic) {
@@ -104,8 +112,9 @@ function mainfunc () {
     }, 1200, function () {
       $screen.fadeIn(1000);
 
-      $('.primary, .primary1').addClass(anim_classes.fadeOutef).one(anim_classes.animationEnd, function () {
-        $('.primary, .primary1').hide();
+      $('.primary, .primary1, #mainTopicBtn').addClass(anim_classes.fadeOutef)
+      .one(anim_classes.animationEnd, function () {
+        $('.primary, .primary1, #mainTopicBtn').hide();
         $('.tertiary,.secondary, .primary2').show();
         $('.tertiary,.secondary, .primary2').addClass(anim_classes.fadeInef);
       })
@@ -119,11 +128,11 @@ function mainfunc () {
 $('.secondary').on('click', function () {
 
   new_arr = pickProperty(shuffled, questions_test).slice(0)
-  duplicate = new_arr.slice(0)
+  
   $experiment.empty()
   getquestion()
   test()
-  console.log('The button \'extend\' is clicked')
+  console.log('New topic button is clicked')
   $topic.addClass(anim_classes.bounceOutLeft).one(anim_classes.animationEnd, function () {
 
     $topic.text(arrayOfTopics)
@@ -133,7 +142,7 @@ $('.secondary').on('click', function () {
 
 $('.new_topic').on('click', function () {
   new_arr = pickProperty(shuffled, questions_test).slice(0)
-  duplicate = new_arr.slice(0)
+  
   $('.whole_page').addClass(anim_classes.fadeOutef).one(anim_classes.animationEnd, function () {
     $(this).removeClass(anim_classes.fadeOutef)
     $(this).hide()
@@ -145,7 +154,7 @@ $('.new_topic').on('click', function () {
   $experiment.empty()
   getquestion()
   test()
-  console.log('The button \'extend\' is clicked')
+  console.log('New topic button is clicked')
   $topic.addClass(anim_classes.bounceOutLeft).one(anim_classes.animationEnd, function () {
     $topic.text(arrayOfTopics)
     $topic.removeClass(anim_classes.bounceOutLeft)
